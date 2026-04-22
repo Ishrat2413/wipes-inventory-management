43e5411648616a69e2e4e73623fea19522669e16
FyslAhmd <info.faysal.32@gmail.com>
2026-04-22 14:41:52 +0600

    feat: Add Push Subscription page and update CRM navigation items

---
+++ app/dashboard/crm/pushSubscription/page.tsx
@@ -0,0 +1,5 @@
+import CrmPushSubscriptionPage from "@/components/dashboard/crm/crm-push-subscription-page";
+
+export default function DashboardCrmProductPage() {
+  return <CrmPushSubscriptionPage />;
+}
